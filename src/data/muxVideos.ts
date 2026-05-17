92b93f36a1f766e5f4ed9c408e62a975fa4ca3b4
Daniel Angelucci <dada@MAC.station>
2026-05-17 18:37:07 +0200

    Fix: correctly map Mux videos to their projects

--- src/data/muxVideos.ts
+++ src/data/muxVideos.ts
@@ -2,19 +2,19 @@
 
 export const muxVideos = {
   "DADA x MARTINLEE": {
-    playbackId: "VDom02f7uOCiQ01KylEnvSxtbrynZAG65vUimyD2iBJJw",
-    hlsUrl: "https://stream.mux.com/VDom02f7uOCiQ01KylEnvSxtbrynZAG65vUimyD2iBJJw.m3u8"
+    playbackId: "zhe8VLsgtuYPQvXncriiaYfRk00HUr1EDc00N9ts3zFnI",
+    hlsUrl: "https://stream.mux.com/zhe8VLsgtuYPQvXncriiaYfRk00HUr1EDc00N9ts3zFnI.m3u8"
   },
   "DADA x MC x PRADA": {
-    playbackId: "kMXmmGSc7qWurBH801UJD0101lhV5TtnH6DttZ02v01pu02bI",
-    hlsUrl: "https://stream.mux.com/kMXmmGSc7qWurBH801UJD0101lhV5TtnH6DttZ02v01pu02bI.m3u8"
-  },
-  "DADA X THEMARKETEXIT": {
     playbackId: "ajsrSGI9dDVTlIEQAIE1N3gCegIA018B8DxBG9ECQJN8",
     hlsUrl: "https://stream.mux.com/ajsrSGI9dDVTlIEQAIE1N3gCegIA018B8DxBG9ECQJN8.m3u8"
   },
+  "DADA X THEMARKETEXIT": {
+    playbackId: "kMXmmGSc7qWurBH801UJD0101lhV5TtnH6DttZ02v01pu02bI",
+    hlsUrl: "https://stream.mux.com/kMXmmGSc7qWurBH801UJD0101lhV5TtnH6DttZ02v01pu02bI.m3u8"
+  },
   "THE SCHOOL FOR MORAL AMBITION — BY RUTGER BREGMAN": {
-    playbackId: "zhe8VLsgtuYPQvXncriiaYfRk00HUr1EDc00N9ts3zFnI",
-    hlsUrl: "https://stream.mux.com/zhe8VLsgtuYPQvXncriiaYfRk00HUr1EDc00N9ts3zFnI.m3u8"
+    playbackId: "VDom02f7uOCiQ01KylEnvSxtbrynZAG65vUimyD2iBJJw",
+    hlsUrl: "https://stream.mux.com/VDom02f7uOCiQ01KylEnvSxtbrynZAG65vUimyD2iBJJw.m3u8"
   }
 };
